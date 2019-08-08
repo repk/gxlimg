51969367c28991bcd53fa7eb3184d025a86bebba
Carlo Caione <ccaione@baylibre.com>
2019-08-08 20:12:40 +0100

    bl2: Fix *_SZ define names
    No functional changes. The *_SZ define names are confusing and sometimes
    misleading. Fix that.
    
    s/BL2IMG_TOTSZ/BL2IMG_SZ/: For the sake of consistency
    s/BL2RAND_SZ/BL2IV_SZ/: Specify that is IV not just random bytes
    s/BL2HDR_SZ/BL2BLKHDR_SZ/: This is the size of the block header, not of
      the whole BL2 (that is 4096 bytes)
    s/BL2HASH_SZ/BL2SIG_SZ/: That's not an hash but a signature
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- bl2.c
+++ bl2.c
@@ -20,15 +20,15 @@
 #define bh_rd(h, sz, off)						\
 	(le ## sz ## toh(*(uint ## sz ## _t *)((h) + off)))
 
-#define BL2IMG_TOTSZ	0xc000
-#define BL2RAND_SZ	0x10
-#define BL2HDR_SZ	0x40
-#define BL2HASH_SZ	0x200
+#define BL2IMG_SZ	0xc000
+#define BL2IV_SZ	0x10
+#define BL2BLKHDR_SZ	0x40
+#define BL2SIG_SZ	0x200
 #define BL2KEY_SZ	0xD80
 #define BL2KEYHDR_SZ	0x30
-#define BL2SHA2_LEN	0x20
-#define BL2BIN_SZ	(BL2IMG_TOTSZ -	(BL2RAND_SZ + BL2HDR_SZ +	\
-			 BL2HASH_SZ + BL2KEYHDR_SZ + BL2KEY_SZ))
+#define BL2SHA2_SZ	0x20
+#define BL2BIN_SZ	(BL2IMG_SZ -	(BL2IV_SZ + BL2BLKHDR_SZ +	\
+			 BL2SIG_SZ + BL2KEYHDR_SZ + BL2KEY_SZ))
 
 #define BL2HDR_MAGIC	(*(uint32_t *)"@AML")
 
@@ -64,10 +64,10 @@ static int gi_bl2_init(struct bl2 *bl2, int fd)
 	}
 	bl2->payloadsz = fsz;
 	bl2->flag = 0; /* Not RSA signature support yet */
-	bl2->hash_start = BL2HDR_SZ + BL2SHA2_LEN;
-	bl2->hash_end = BL2HASH_SZ + BL2KEYHDR_SZ + BL2KEY_SZ + BL2BIN_SZ -
-		BL2SHA2_LEN;
-	bl2->totlen = BL2HDR_SZ + BL2HASH_SZ + BL2KEYHDR_SZ +BL2KEY_SZ +
+	bl2->hash_start = BL2BLKHDR_SZ + BL2SHA2_SZ;
+	bl2->hash_end = BL2SIG_SZ + BL2KEYHDR_SZ + BL2KEY_SZ + BL2BIN_SZ -
+		BL2SHA2_SZ;
+	bl2->totlen = BL2BLKHDR_SZ + BL2SIG_SZ + BL2KEYHDR_SZ +BL2KEY_SZ +
 		BL2BIN_SZ;
 
 	return 0;
@@ -123,8 +123,8 @@ out:
 
 static int gi_bl2_dump_hdr(struct bl2 const *bl2, int fd)
 {
-	uint8_t hdr[BL2HDR_SZ] = {};
-	uint8_t rd[BL2RAND_SZ];
+	uint8_t hdr[BL2BLKHDR_SZ] = {};
+	uint8_t rd[BL2IV_SZ];
 	ssize_t nr;
 	off_t off;
 	int ret;
@@ -135,7 +135,7 @@ static int gi_bl2_dump_hdr(struct bl2 const *bl2, int fd)
 		goto out;
 	}
 
-	ret = gi_random(rd, BL2RAND_SZ);
+	ret = gi_random(rd, BL2IV_SZ);
 	if(ret < 0)
 		goto out;
 
@@ -146,18 +146,18 @@ static int gi_bl2_dump_hdr(struct bl2 const *bl2, int fd)
 	}
 	bh_wr(hdr, 32, 0x00, BL2HDR_MAGIC);
 	bh_wr(hdr, 16, 0x04, bl2->totlen);
-	bh_wr(hdr, 16, 0x08, BL2HDR_SZ);
+	bh_wr(hdr, 16, 0x08, BL2BLKHDR_SZ);
 	bh_wr(hdr, 8,  0x0a, 1);
 	bh_wr(hdr, 8,  0x0b, 1);
 	bh_wr(hdr, 32, 0x10, 0); /* SHA256 signature, no RSA */
-	bh_wr(hdr, 32, 0x14, BL2HDR_SZ); /* HDR size */
-	bh_wr(hdr, 32, 0x18, BL2HASH_SZ);
+	bh_wr(hdr, 32, 0x14, BL2BLKHDR_SZ); /* HDR size */
+	bh_wr(hdr, 32, 0x18, BL2SIG_SZ);
 	bh_wr(hdr, 16, 0x1c, bl2->hash_start); /* Beginning of hashed payload */
 	bh_wr(hdr, 32, 0x20, 0); /* Null RSA KEY type */
-	bh_wr(hdr, 16, 0x24, BL2HDR_SZ + BL2HASH_SZ); /* RSA KEY Offset */
+	bh_wr(hdr, 16, 0x24, BL2BLKHDR_SZ + BL2SIG_SZ); /* RSA KEY Offset */
 	bh_wr(hdr, 32, 0x28, BL2KEYHDR_SZ + BL2KEY_SZ);
 	bh_wr(hdr, 16, 0x2c, bl2->hash_end);
-	bh_wr(hdr, 16, 0x34, BL2HDR_SZ + BL2HASH_SZ + BL2KEYHDR_SZ + BL2KEY_SZ); /* Payload offset */
+	bh_wr(hdr, 16, 0x34, BL2BLKHDR_SZ + BL2SIG_SZ + BL2KEYHDR_SZ + BL2KEY_SZ); /* Payload offset */
 	bh_wr(hdr, 16, 0x38, BL2BIN_SZ);
 
 	nr = gi_bl2_write_blk(fd, rd, sizeof(rd));
@@ -189,7 +189,7 @@ static int gi_bl2_dump_key(struct bl2 const *bl2, int fd)
 		return -EINVAL;
 	}
 
-	off = lseek(fd, BL2RAND_SZ + BL2HDR_SZ + BL2HASH_SZ + 0x18, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ + BL2BLKHDR_SZ + BL2SIG_SZ + 0x18, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
@@ -198,7 +198,7 @@ static int gi_bl2_dump_key(struct bl2 const *bl2, int fd)
 	gi_bl2_write_blk(fd, (uint8_t *)(&val), 4);
 
 	/* TODO What is this offset */
-	off = lseek(fd, BL2RAND_SZ + 0x8ec, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ + 0x8ec, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
@@ -207,7 +207,7 @@ static int gi_bl2_dump_key(struct bl2 const *bl2, int fd)
 	gi_bl2_write_blk(fd, (uint8_t *)(&val), 4);
 
 	/* TODO What is this offset */
-	off = lseek(fd, BL2RAND_SZ + 0xb20, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ + 0xb20, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
@@ -234,7 +234,7 @@ static int gi_bl2_dump_binary(struct bl2 const *bl2, int fout, int fin)
 		SEEK_ERR(off, ret);
 		goto out;
 	}
-	off = lseek(fout, BL2RAND_SZ + BL2HDR_SZ + BL2HASH_SZ + BL2KEYHDR_SZ +
+	off = lseek(fout, BL2IV_SZ + BL2BLKHDR_SZ + BL2SIG_SZ + BL2KEYHDR_SZ +
 			BL2KEY_SZ, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
@@ -264,7 +264,7 @@ static int gi_bl2_sign(struct bl2 const *bl2, int fd)
 {
 	EVP_MD_CTX *ctx;
 	uint8_t tmp[1024];
-	uint8_t hash[BL2SHA2_LEN];
+	uint8_t hash[BL2SHA2_SZ];
 	size_t i;
 	ssize_t nr;
 	off_t off;
@@ -283,13 +283,13 @@ static int gi_bl2_sign(struct bl2 const *bl2, int fd)
 	}
 
 	/* Hash header */
-	off = lseek(fd, BL2RAND_SZ, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
 	}
-	nr = gi_bl2_read_blk(fd, tmp, BL2HDR_SZ);
-	if((nr < 0) || (nr != BL2HDR_SZ)) {
+	nr = gi_bl2_read_blk(fd, tmp, BL2BLKHDR_SZ);
+	if((nr < 0) || (nr != BL2BLKHDR_SZ)) {
 		PERR("Cannot read header from fd %d: ", fd);
 		ret = (int)nr;
 		goto out;
@@ -301,7 +301,7 @@ static int gi_bl2_sign(struct bl2 const *bl2, int fd)
 	}
 
 	/* Hash payload */
-	off = lseek(fd, BL2RAND_SZ + bl2->hash_start, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ + bl2->hash_start, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
@@ -327,13 +327,13 @@ static int gi_bl2_sign(struct bl2 const *bl2, int fd)
 	}
 
 	/* Only SHA256 signature is supported so far */
-	off = lseek(fd, BL2RAND_SZ + BL2HDR_SZ, SEEK_SET);
+	off = lseek(fd, BL2IV_SZ + BL2BLKHDR_SZ, SEEK_SET);
 	if(off < 0) {
 		SEEK_ERR(off, ret);
 		goto out;
 	}
-	nr = gi_bl2_write_blk(fd, hash, BL2SHA2_LEN);
-	if(nr != BL2SHA2_LEN) {
+	nr = gi_bl2_write_blk(fd, hash, BL2SHA2_SZ);
+	if(nr != BL2SHA2_SZ) {
 		PERR("Cannot write SHA sig in fd %d:", fd);
 		ret = (int)nr;
 		goto out;
@@ -375,7 +375,7 @@ int gi_bl2_create_img(char const *fin, char const *fout)
 		goto out;
 
 	/* Fill the whole file with zeros */
-	ret = ftruncate(fdout, bl2.totlen + BL2RAND_SZ);
+	ret = ftruncate(fdout, bl2.totlen + BL2IV_SZ);
 	if(ret < 0)
 		goto out;
 
